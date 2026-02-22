45159ee67bfed694201a4a321d31866c0e7cf286
Radu Edward-Andrei <raduedwardandrei@gmail.com>
2026-02-22 22:31:47 +0200

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Radu Edward-Andrei on 22.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
